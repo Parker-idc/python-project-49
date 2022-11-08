56dbaf76c085452eddcede094139314faf53bd25
Parker-idc <egor.kruchenkov@gmail.com>
2022-11-08 12:18:45 +0300

    fix prime.py

--- brain_game/games/prime.py
+++ brain_game/games/prime.py
@@ -7,13 +7,13 @@ TITLE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def is_prime(num):
     divider = 0
-    for i in range(1, (num + 1) // 2):
+    for i in range(1, (num + 2) // 2):
         if num % i == 0:
             divider += 1
-        elif divider == 1:
-            return True
-        else:
-            return False
+    if divider == 1:
+        return True
+    else:
+        return False
 
 
 def get_game_process():
